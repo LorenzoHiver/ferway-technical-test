545512f7ce35ea82c3507c7ce89b7a3434d9722c
Lorenzo Hiver <lorenzohiver1@gmail.com>
2022-08-02 23:48:39 +0200

    fix(IconSmall): remove debug

--- src/atoms/IconSmall.jsx
+++ src/atoms/IconSmall.jsx
@@ -1,15 +1,9 @@
 import React, { useEffect } from "react"
 
 const IconSmall = ({ url, metro, ...props }) => {
-
-    useEffect(() => {
-        console.log(url)
-    })
-
     return (
         <img className="w-6 h-6" src={url} alt={`metro_ligne_${metro}`} {...props} />
     )
-
 }
 
 export default IconSmall
